f0ec6391987b009231cbeac78309a5effdc1852f
Zhaoxizhen <zhaoxizhen94@gmail.com>
2020-07-27 11:07:52 +0800

    eight queens stack overflow

--- eight_queens.py
+++ eight_queens.py
@@ -2,6 +2,8 @@ import sys
 
 
 def print_queen(arr):
+    global count
+    print('找到第', count, '个解:', arr)
     for i in range(8):
         for j in range(8):
             if arr[i] == j:
@@ -11,51 +13,51 @@ def print_queen(arr):
         print('')
 
 
-global arr
-arr = []
+global count
+count = 0
 
 
-def queen(x, y):
-    if len(arr) == 0 and x == 0 and y >= 8:
-        return
-    if len(arr) >= 8:  # 基线条件:列表摆满8个 寻找下一个可能
-        print('找到答案:')
+def queen(num, arr, n):
+    global count
+    if len(arr) == num:  # 基线条件:放满num个
+        count += 1
         print_queen(arr)
-        yield arr
-        y = arr.pop()
-        for i in queen(len(arr), y+1):
-            yield i
-    if y >= 8:  # 递归条件:无法在同一行里摆下,返回上一行
-        y = arr.pop()
-        for i in queen(len(arr), y+1):
-            yield i
-    arr.append(y)  # 置入一个位置为x行,y列的皇后
-    if check_queen(arr):  # 判断是否冲突,不冲突则进入下一行第一个位置
-        for i in queen(len(arr), 0):
-            yield i
-    else:  # 冲突则往后移一个位置
-        y = arr.pop()
-        for i in queen(len(arr), y+1):
-            yield i
+        # yield arr
+        n = arr.pop() + 1
+        if len(arr) == 0 and n >= num:
+            return
+        return queen(num, arr, n)
+    else:
+        if n >= num:  # 如果在此行都没有找到合适位置,则返回上一层,尝试下一个位置
+            p = arr.pop() + 1
+            return queen(num, arr, p)
+        else:
+            for i in range(n, num):  # 在同一行最多需要尝试num次
+                if check_queen(arr, i):  # 找到不冲突的位置则尝试放下一个
+                    arr.append(i)
+                    return queen(num, arr, 0)  # 进入下一行 位置从0开始
+            p = arr.pop() + 1  # 如果num次循环仍然没有找到合适的位置,则返回上一层,尝试下一个位置
+            return queen(num, arr, p)
 
 
-def check_queen(arr):  # 检测最后一个皇后的位置是否冲突
-    index = len(arr) - 1
-    if len(arr) == 1:  # 只有一个的时候返回True
-        return True
-    for i in range(index):
-        if abs(arr[i] == arr[index]):  # 在同一列
+def check_queen(arr, y):  # 检测将要放入的皇后位置是否冲突
+    index = len(arr)
+    for i in range(index):  # index=0时不进入循环直接返回True
+        if abs(arr[i] == y):  # 在同一列
             return False
-        if abs(arr[i] - arr[index]) == abs(i - index):  # 在同一斜行
+        if abs(arr[i] - y) == abs(i - index):  # 在同一斜行
             return False
     return True
 
 
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(100000000)
 # result1 = list(queen([], 0, 0))  # 从第一行第一列开始摆下
 # print_queen(result[0])
-for i1 in queen(0, 0):
-    print(i1)
+# i1 = queen(8, [], 0)
+# for i1 in queen(8, [], 0):
+#     print(i1)
+# print(i1)
+queen(8, [], 0)
 
 
 # result = queen(arr0, 0, 0)
