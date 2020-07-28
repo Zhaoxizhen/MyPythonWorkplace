4ff3a2d9f5ef576e550371986fca27c386cd24c2
Zhaoxizhen <zhaoxizhen94@gmail.com>
2020-07-28 15:56:09 +0800

    module fileinput test

--- file_input_test.py
+++ file_input_test.py
@@ -1,6 +1,8 @@
-import fileinput                                   #  1
-                                                   #  2
-for line in fileinput.input(inplace=True):         #  3
-    line = line.rstrip()                           #  4
-    num = fileinput.lineno()                       #  5
-    print('{:<50} # {:2d}'.format(line, num))      #  6
+import fileinput                                   # 1
+                                                   # 2
+for line in fileinput.input(inplace=True):         # 3
+    line = line.rstrip()                           # 4
+    num = fileinput.lineno()                       # 5
+    print('{:<50} #{:2d}'.format(line, num))       # 6 左对齐的50个字符用line填充 2d使用2位十进制数num填充
+
+
